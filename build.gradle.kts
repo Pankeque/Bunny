6d3301f56d663ee6ce404140facbf9b7b4264692
Pankeque <danfaz39@gmail.com>
2026-07-26 22:55:38 -0300

    Atualizar o build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Raiz)
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("com.android.library") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false // Atualizado
-    id("com.google.dagger.hilt.android") version "2.48" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false // Atualizado
+    id("com.android.application") version "8.5.0" apply false // Recomendo atualizar o AGP também
+    id("com.android.library") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false // <--- ADICIONE ISSO
+    id("com.google.dagger.hilt.android") version "2.51" apply false // Atualize o Hilt
+    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false // Atualize o KSP
 }
