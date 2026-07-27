169bf95244bbd11f047710f79023b44df1549cab
Pankeque <danfaz39@gmail.com>
2026-07-26 22:55:57 -0300

    Atualizar o build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.5.0" apply false // Recomendo atualizar o AGP também
     id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false // <--- ADICIONE ISSO
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("com.google.dagger.hilt.android") version "2.51" apply false // Atualize o Hilt
     id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false // Atualize o KSP
 }
